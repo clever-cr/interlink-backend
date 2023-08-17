717fcdeb70d0fe5006de66e81f9e7a6ecb1b505d
clever-cr <cleverumuhuza@gmail.com>
2023-08-17 13:39:16 +0200

    fix issue

--- src/migrations/20230807093452-create-user.js
+++ src/migrations/20230807093452-create-user.js
@@ -21,7 +21,9 @@ module.exports = {
       lastName: {
         type: Sequelize.STRING,
       },
-
+      title: {
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
